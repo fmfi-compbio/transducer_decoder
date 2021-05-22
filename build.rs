081df6dc64fe8c4df0ae92539d8f324054d5b682
usama <bozavlado@gmail.com>
2021-05-22 19:27:55 +0200

    fix build

--- build.rs
+++ build.rs
@@ -4,9 +4,7 @@ use std::process::Command;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    if !Path::new(&format!("{}/lib/libmkl_core.a", &out_dir)).exists() ||
-       !Path::new(&format!("{}/lib/libmkl_sequential.a", &out_dir)).exists() ||
-       !Path::new(&format!("{}/lib/libmkl_intel_ilp64.a", &out_dir)).exists() {
+    if !Path::new(&format!("{}/lib/libmerged.a", &out_dir)).exists() {
         Command::new("wget")
                 .arg("https://anaconda.org/intel/mkl-static/2020.0/download/linux-64/mkl-static-2020.0-intel_166.tar.bz2")
 //                .arg("https://anaconda.org/intel/mkl-static/2020.1/download/linux-64/mkl-static-2020.1-intel_217.tar.bz2")
@@ -22,10 +20,10 @@ fn main() {
 
         Command::new("ar")
                 .arg("-rcT")
-                .arg(&format!("{}/libmerged.a", out_dir))
-                .arg(&format!("{}/libmkl_sequential.a", out_dir))
-                .arg(&format!("{}/libmkl_core.a", out_dir))
-                .arg(&format!("{}/libmkl_intel_ilp64.a", out_dir))
+                .arg(&format!("{}/lib/libmerged.a", out_dir))
+                .arg(&format!("{}/lib/libmkl_sequential.a", out_dir))
+                .arg(&format!("{}/lib/libmkl_core.a", out_dir))
+                .arg(&format!("{}/lib/libmkl_intel_ilp64.a", out_dir))
                 .status().unwrap();
     }
 
